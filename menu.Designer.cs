9c1607112213a89d923f5372c235d6e219b329dd
Veronika.Koshtur <nikakoshtur@gmail.com>
2017-05-24 14:51:18 +0200

    menu.cs

---
+++ menu.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace sudoku
+{
+	partial class menu
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(menu));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.IndianRed;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Location = new System.Drawing.Point(190, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.IndianRed;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button4.Location = new System.Drawing.Point(190, 474);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(200, 70);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.Button4Click);
+            // 
+            // menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(580, 558);
+            this.ControlBox = false;
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Location = new System.Drawing.Point(800, 600);
+            this.MinimumSize = new System.Drawing.Size(569, 453);
+            this.Name = "menu";
+            this.Text = "Sudoku";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ResumeLayout(false);
+
+		}
+        private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button1;
+	}
+}
---
+++ menu.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace sudoku
+{
+	/// <summary>
+	/// Description of menu.
+	/// </summary>
+	public partial class menu : Form
+	{
+
+		private bool cen=true;
+		public menu()
+		{
+			
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+
+			this.DialogResult= DialogResult.OK;
+		}
+		
+		void Button3Click(object sender, EventArgs e)
+		{
+			Rules aut=new Rules();
+			this.Hide();
+			if(aut.ShowDialog()== DialogResult.OK)
+			{
+				this.Visible=true;
+				
+			}else this.Visible=true;
+		}
+		
+		void Button4Click(object sender, EventArgs e)
+		{
+            DialogResult dr = MessageBox.Show("Do you want to finish the game?", "Exit", MessageBoxButtons.YesNo);
+            if (dr == DialogResult.Yes)
+            { this.DialogResult= DialogResult.Cancel;}
+		
+			
+		}
+	}
+}
